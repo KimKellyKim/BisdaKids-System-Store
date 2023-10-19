2ff25b035e4e1a80fa585f2ca43e9c942f608c70
KimKellyKim <paraderok9@gmail.com>
2023-10-19 17:17:20 +0800

    purchase modal

--- src/components/gameStoreComponents/ViewGameStore.jsx
+++ src/components/gameStoreComponents/ViewGameStore.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { supabase } from '../../supabase-config'
-import Loading from "../Loading"
-
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import Modal from '@mui/material/Modal'
+import React, { useState, useEffect } from "react";
+import { supabase } from '../../supabase-config';
+import Loading from "../Loading";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const style = {
   position: 'absolute',
@@ -21,50 +20,110 @@ const style = {
 };
 
 function ViewGameStore({ checker, setChecker }) {
-  const [loading, setLoading] = useState(false)
-  const [gameStoreData, setGameStoreData] = useState([])
-  const [itemNames, setItemNames] = useState({})
-  const [open, setOpen] = useState(false)
-  const [selectedItemData, setSelectedItemData] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [gameStoreData, setGameStoreData] = useState([]);
+  const [itemNames, setItemNames] = useState({});
+  const [open, setOpen] = useState(false);
+  const [selectedItemData, setSelectedItemData] = useState(null);
+  const [username, setUsername] = useState('');
+  const [errorModal, setErrorModal] = useState(false);
+  const [errorText, setErrorText] = useState('');
+  const [successModal, setSuccessModal] = useState(false);
+  const [successText, setSuccessText] = useState('');
 
   useEffect(() => {
-    fetchGameStore()
-    fetchItemNames()
-  }, [checker])
+    fetchGameStore();
+    fetchItemNames();
+  }, [checker]);
 
   const fetchGameStore = async () => {
-    setLoading(true)
+    setLoading(true);
     const { data, error } = await supabase
       .from('system_store')
-      .select('*')
+      .select('*');
 
     if (data) {
-      setGameStoreData(data)
+      setGameStoreData(data);
+    } else {
+      setErrorText('Failed to fetch game store data. Please try again later.');
+      setErrorModal(true);
     }
-    error && console.error(error)
-    setLoading(false)
+    error && console.error(error);
+    setLoading(false);
   }
 
   const fetchItemNames = async () => {
     const { data, error } = await supabase
       .from('items')
-      .select('item_id, item_name')
+      .select('item_id, item_name');
 
     if (data) {
-      const itemNamesMap = {}
-      data.forEach(item => { itemNamesMap[item.item_id] = item.item_name; })
-      setItemNames(itemNamesMap)
+      const itemNamesMap = {};
+      data.forEach(item => { itemNamesMap[item.item_id] = item.item_name; });
+      setItemNames(itemNamesMap);
+    } else {
+      setErrorText('Failed to fetch item names. Please try again later.');
+      setErrorModal(true);
     }
-    error && console.error(error)
+    error && console.error(error);
   }
 
   const handleOpen = (data) => {
-    setSelectedItemData(data)
-    setOpen(true)
+    setSelectedItemData(data);
+    setOpen(true);
   }
 
   const handleClose = () => {
-    setSelectedItemData(null)
+    setSelectedItemData(null);
+    setOpen(false);
+  }
+
+  // Function to fetch the user_id based on the entered username
+  const fetchUserIdByUsername = async (username) => {
+    const { data, error } = await supabase
+      .from('user_account')
+      .select('user_id')
+      .eq('user_name', username)
+      .single();
+
+    if (data) {
+      console.error("Success")
+      return data.user_id;
+    } else {
+      setErrorText("Username not found. Please try again")
+      setErrorModal(true);
+      console.error("Username not found");
+      return null;
+    }
+  }
+
+  const handleBuy = async () => {
+    if (selectedItemData && username) {
+      const user_id = await fetchUserIdByUsername(username);
+
+      if (user_id !== null) {
+        const store_offer_id = selectedItemData.id;
+
+        if (store_offer_id !== null) {
+          const { data, error } = await supabase
+            .from('system_transactions')
+            .insert([
+              {
+                store_offer_id: selectedItemData.store_offer_id,
+                user_id: user_id,
+                time_stamp: new Date(),
+              }
+            ])
+          setSuccessText("Purchase Successful")
+          setSuccessModal(true)
+        } 
+        else {
+          setErrorText('Store offer not found. Please try again later.')
+          setErrorModal(true)
+          console.error('Store offer not found')
+        }
+      }
+    }
     setOpen(false)
   }
 
@@ -74,30 +133,30 @@ function ViewGameStore({ checker, setChecker }) {
         <Loading />
       ) : (
         <>
-        <div className="w-4/6 max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {gameStoreData.map(data => (
-            <a
-              onClick={() => handleOpen(data)}
-              key={data.id}
-              className="container hover:shadow-xl hover:shadow-orange-200 cursor-pointer rounded-lg overflow-hidden transition-transform transform hover:scale-105"
-            >
-              <img
-                className="w-full h-48 object-cover"
-                src="https://tecdn.b-cdn.net/img/new/standard/city/044.webp"
-                alt="Skyscrapers"
-              />
-              <div className="p-4">
-                <h5 className="text-xl font-semibold text-gray-900">
-                  {itemNames[data.item_id] || 'Item Not Found'}
-                </h5>
-                <p className="text-gray-600">Php {data.price}</p>
-                <p className="mt-2 text-gray-800">
-                  Quantity: {data.offer_quantity}
-                </p>
-              </div>
-            </a>
-          ))}
-        </div>
+          <div className="w-4/6 max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {gameStoreData.map(data => (
+              <a
+                onClick={() => handleOpen(data)}
+                key={data.id} // Make sure this key is unique
+                className="container hover:shadow-xl hover:shadow-orange-200 cursor-pointer rounded-lg overflow-hidden transition-transform transform hover:scale-105"
+              >
+                <img
+                  className="w-full h-48 object-cover"
+                  src="https://tecdn.b-cdn.net/img/new/standard/city/044.webp"
+                  alt="Skyscrapers"
+                />
+                <div className="p-4">
+                  <h5 className="text-xl font-semibold text-gray-900">
+                    {itemNames[data.item_id] || 'Item Not Found'}
+                  </h5>
+                  <p className="text-gray-600">Php {data.price}</p>
+                  <p className="mt-2 text-gray-800">
+                    Quantity: {data.offer_quantity}
+                  </p>
+                </div>
+              </a>
+            ))}
+          </div>
         </>
       )}
       <Modal
@@ -110,24 +169,23 @@ function ViewGameStore({ checker, setChecker }) {
           <button
             onClick={handleClose}
             type="button"
-            className="absolute top-4 right-5 text-3xl p-1 rounded-full hover:bg-gray-400 duration-150"
+            className="absolute top-4 right-5 text-3xl p-1 rounded-full hover-bg-gray-400 duration-150"
           >
             <AiOutlineCloseCircle />
           </button>
           <Typography id="modal-modal-title">
-            <h1 className="text-2xl font-bold text-center text-green-500">
+            <div className="text-2xl font-bold text-center text-green-500">
               Purchase
-            </h1>
+            </div>
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             <div className="py-2">
-              <div>
                 {selectedItemData && (
                   <form className="flex flex-col p-4">
                     <div className="flex flex-col gap-4">
                       <label className="text-lg font-semibold" htmlFor="itemName">
                         Item Name: {itemNames[selectedItemData.item_id] || 'Item Not Found'}
-                      </label>               
+                      </label>
                       <label className="text-lg font-semibold" htmlFor="itemDesc">
                         Quantity: {selectedItemData.offer_quantity}
                       </label>
@@ -135,20 +193,40 @@ function ViewGameStore({ checker, setChecker }) {
                         Total Price: Php {selectedItemData.price}
                       </label>
                       <label htmlFor="bundleQuan" className='text-lg font-semibold'>Username:</label>
-                        <input 
-                            className='outline-none border-2 focus:border-gray-400 rounded-md text-center p-1' 
-                        type="text" />
-                        <div className="items-center ">
-                            <Button variant='contained'>BUY</Button>
-                        </div>     
-                    </div>             
+                      <input
+                        className='outline-none border-2 focus-border-gray-400 rounded-md text-center p-1'
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                      <Button variant='contained' onClick={handleBuy}>BUY</Button>
+                    </div>
                   </form>
                 )}
-              </div>
             </div>
           </Typography>
         </Box>
       </Modal>
+      {errorModal && ( 
+        <div className='fixed top-0 left-0 p-5 w-full h-screen flex justify-center items-center bg-gray-600 bg-opacity-50 z-40'>
+          <div className='flex flex-col items-center gap-5 p-5 bg-white shadow-2xl rounded-md'>
+            <h1 className='text-4xl font-bold text-red-500'>{errorText}</h1>
+            <button className='p-1 hover-bg-red-500 rounded-full duration-150' type='button' onClick={() => setErrorModal(false)}>
+              <p className='text-4xl'> <AiOutlineCloseCircle /> </p>
+            </button>
+          </div>
+        </div>
+      )}
+      {successModal && (
+        <div className='fixed top-0 left-0 p-5 w-full h-screen flex justify-center items-center bg-gray-600 bg-opacity-50 z-40'>
+          <div className='flex flex-col items-center gap-5 p-5 bg-white shadow-2xl rounded-md'>
+            <h1 className='text-4xl font-bold text-green-500'>{successText}</h1>
+            <button className='p-1 hover-bg-red-500 rounded-full duration-150' type='button' onClick={() => setSuccessModal(false)}>
+              <p className='text-4xl'> <AiOutlineCloseCircle /> </p>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/GameStore.jsx
+++ src/pages/GameStore.jsx
@@ -8,7 +8,7 @@ function GameStore(){
             <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div className="p-6 bg-white border-b border-gray-200">
                 <h1 className="text-3xl font-semibold text-gray-800">
-                  BISDAKIDS: GAME STORE
+                  BISDAKIDS: SYSTEM STORE
                 </h1><br/><br />
                 <ViewGameStore checker={checker} setChecker={setChecker} />
               </div>
